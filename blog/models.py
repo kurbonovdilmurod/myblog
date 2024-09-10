4380b3f4cd3d663589dcda82bf1bbee2d8221f3b
kurbonovdilmurod <dilmurodkurbonov653@gmail.com>
2024-09-10 12:49:34 +0500

    initial blog code

---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    categories = models.ManyToManyField('Category', related_name='posts')
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} on '{self.post}'"
