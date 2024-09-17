5be90e4561b61e045275716bd34993530e252cea
kurbonovdilmurod <dilmurodkurbonov653@gmail.com>
2024-09-17 15:36:08 +0500

    changed models.py and admin.py in blog

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from blog.models import Category, Comment, Post
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {'slug': ('name',)}
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'status', 'created_on', 'updated_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
 
 class CommentAdmin(admin.ModelAdmin):
     pass
@@ -13,4 +16,3 @@ class CommentAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
-
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
 
     class Meta:
         verbose_name_plural = "categories"
@@ -10,12 +12,23 @@ class Category(models.Model):
         return self.name
 
 
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish"),
+)
 class Post(models.Model):
     categories = models.ManyToManyField('Category', related_name='posts')
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
